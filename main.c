da5f6726ac72b6a96a1aaf29296649c71fc964ac
Denis Alicic <a.denis96@gmail.com>
2017-12-03 19:59:21 +0100

    Ovo je probno komitovanje,napisan osvoni Makefile i jednostavno iscrtavanje kocke

---
+++ main.c
@@ -0,0 +1,87 @@
+#include <stdlib.h>
+#include <GL/glut.h>
+
+/* Dimenzije prozora */
+static int window_width, window_height;
+
+/* Deklaracije callback funkcija. */
+static void on_keyboard(unsigned char key, int x, int y);
+static void on_reshape(int width, int height);
+static void on_display(void);
+
+int main(int argc, char **argv)
+{
+    /* Inicijalizuje se GLUT. */
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
+
+    /* Kreira se prozor. */
+    glutInitWindowSize(600, 600);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow(argv[0]);
+
+    /* Registruju se callback funkcije. */
+    glutKeyboardFunc(on_keyboard);
+    glutReshapeFunc(on_reshape);
+    glutDisplayFunc(on_display);
+
+    /* Obavlja se OpenGL inicijalizacija. */
+    glClearColor(0.75, 0.75, 0.75, 0);
+    glEnable(GL_DEPTH_TEST);
+    glLineWidth(2);
+
+    /* Program ulazi u glavnu petlju. */
+    glutMainLoop();
+
+    return 0;
+}
+
+static void on_keyboard(unsigned char key, int x, int y)
+{
+    switch (key) {
+    case 27:
+        /* Zavrsava se program. */
+        exit(0);
+        break;
+    }
+}
+
+static void on_reshape(int width, int height)
+{
+    /* Pamte se sirina i visina prozora. */
+    window_width = width;
+    window_height = height;
+}
+
+static void on_display(void)
+{
+    /* Brise se prethodni sadrzaj prozora. */
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    /* Podesava se viewport. */
+    glViewport(0, 0, window_width, window_height);
+
+    /* Podesava se projekcija. */
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(
+            60,
+            window_width/(float)window_height,
+            -6, 6);
+
+    /* Podesava se tacka pogleda. */
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(
+            4.77, 1.95, 4.77,
+            0, 1.95 , 0,
+            0, 1, 0
+        );
+    glColor3f(1,0,0);
+    glutSolidCube(1);
+    
+    
+
+    /* Nova slika se salje na ekran. */
+    glutSwapBuffers();
+}
