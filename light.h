adfcf0e74898fa40aa62421897d2bea68e9f32ea
Denis Alicic <a.denis96@gmail.com>
2018-03-17 18:09:13 +0100

    Reorganizovan repozitorijum

--- light.h
+++
@@ -1,6 +0,0 @@
-#ifndef _LIGHT_H_
-#define _LIGHT_H_
-
-void set_light();
-
-#endif
--- scene.h
+++
@@ -1,9 +0,0 @@
-#ifndef SCENE_H
-#define SCENE_H
-#include "variables.h"
-void draw_ball(Position_of_shooting p_shoot);
-void draw_hall();
-void draw_basket();
-void set_postion_of_camera(Position_of_shooting p_shoot);
-int score();
-#endif
